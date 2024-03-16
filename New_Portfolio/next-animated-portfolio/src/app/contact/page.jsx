c4f1c0a16be40b799138fb59af30ffc9359d8eb8
Taha Ahmed 123618 <tahaahmedanees2@gmail.com>
2024-03-17 03:06:50 +0500

    Email Js issue solve

--- New_Portfolio/next-animated-portfolio/src/app/contact/page.jsx
+++ New_Portfolio/next-animated-portfolio/src/app/contact/page.jsx
@@ -7,31 +7,42 @@ import { host } from "@/utils/host";
 const ContactPage = () => {
   const [success, setSuccess] = useState(false);
   const [error, setError] = useState(false);
+  const [lodaing, setLoading] = useState(false);
   const text = "Say Hello";
 
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
-    setError(false);
-    setSuccess(false);
 
-    emailjs
-      .sendForm(
-        process.env.NEXT_PUBLIC_SERVICE_ID,
-        process.env.NEXT_PUBLIC_TEMPLATE_ID,
-        form.current,
-        process.env.NEXT_PUBLIC_PUBLIC_KEY
-      )
-      .then(
-        () => {
-          setSuccess(true);
-          form.current.reset();
-        },
-        () => {
-          setError(true);
-        }
-      );
+    try {
+      setLoading(true);
+      setError(false);
+      setSuccess(false);
+
+      emailjs
+        .sendForm(
+          process.env.NEXT_PUBLIC_SERVICE_ID,
+          process.env.NEXT_PUBLIC_TEMPLATE_ID,
+          form.current,
+          process.env.NEXT_PUBLIC_PUBLIC_KEY
+        )
+        .then(
+          () => {
+            setSuccess(true);
+            form.current.reset();
+          },
+          () => {
+            setError(true);
+            setLoading(false);
+          }
+        );
+    } catch (error) {
+      console.log("Email", error);
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -73,21 +84,55 @@ const ContactPage = () => {
             rows={6}
             className="bg-transparent border-b-2 border-b-black outline-none resize-none"
             name="user_message"
+            placeholder="Message"
           />
           <span>{host?.address}:</span>
           <input
             name="user_email"
             type="text"
             className="bg-transparent border-b-2 border-b-black outline-none"
+            placeholder="Email"
           />
           <span>Regards</span>
-          <button className="bg-purple-200 rounded font-semibold text-gray-600 p-4">
-            Send
+          <button className="bg-black rounded font-bold text-white p-4">
+            {lodaing ? (
+              <>
+                <div
+                  className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+                  role="status"
+                >
+                  <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                    Loading...
+                  </span>
+                </div>
+              </>
+            ) : (
+              "Send"
+            )}
           </button>
-          {success && (
-            <span className="text-green-600 font-semibold">
+          {/* <span className="text-green-600 font-semibold">
               {host?.message}
-            </span>
+            </span> */}
+          {success && (
+            <div
+              className="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md"
+              role="alert"
+            >
+              <div className="flex">
+                <div className="py-1">
+                  <svg
+                    className="fill-current h-6 w-6 text-teal-500 mr-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
+                  </svg>
+                </div>
+                <div>
+                  <p className="font-bold">{host?.message}</p>
+                </div>
+              </div>
+            </div>
           )}
           {error && (
             <span className="text-red-600 font-semibold">{host?.error}</span>
